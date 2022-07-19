5ac11a430b4c0bd4cdc1de9ddb9866d0939c5f13
caue <cauecatonesilva@gmail.com>
2022-07-19 15:29:23 -0400

    atualizacao-0.1

---
+++ login/login.js
@@ -0,0 +1,9 @@
+function enviar(){
+    if (form.nome.value == "")
+        {alert ("Não há texto escrito") 
+    } else if(form.senha.value ==""){
+        alert("não há senha digitada")
+    } else{
+        alert ("cadastro concluido com sucesso")
+    }
+}
